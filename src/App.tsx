3ef56f17231ef98fac9a12b27f1b0e3a6254d7e6
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-04-24 15:17:30 +0200

    demonstrate type definitions of event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
+import Button from './components/Button';
 import Greet from './components/Greet';
 import Heading from './components/Heading';
+import Input from './components/Input';
 import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
@@ -29,14 +31,20 @@ function App() {
 
   return (
     <div className="App">
-      <Greet name="Slavcho" isLoggedIn={false} />
+      {/* <Greet name="Slavcho" isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
       <Status status='loading' />
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
+      </Oscar> */}
+
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id)
+      }} />
+
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   );
 }
